fab75e1a4371f305333174eac54384cd057b85e0
korkutemir <55914306+korkutemir@users.noreply.github.com>
2023-05-04 16:54:38 +0300

    korkut

--- index.js
+++ index.js
@@ -34,35 +34,29 @@ app.listen(port, function () {
 
 
 const puppeteer = require('puppeteer');
-var dizin=["iPhone 4","iPhone 6","iPhone 5","iPhone 7","iPad","iPad Pro","iPhone X","iPhone 11","iPhone 12","iPhone 13","Pixel 2",
-"Pixel 3","Pixel 4","Pixel 5","Nexus 4","Nexus 5","Nexus 6","Nexus 7","Nexus 10","Galaxy S5","iPad Mini",
-"Galaxy Note 3","Galaxy S8","Galaxy S9+","Galaxy S5","Nokia N9"];
+var dizin=["iPhone 4","iPhone 6","iPhone 5","iPhone 7","iPhone 6","iPad","iPad Pro","iPhone X","iPhone 11","iPhone 12","iPhone 13","Pixel 2",
+"Pixel 3","iPad Pro","Pixel 4","Pixel 5","iPad Pro","Nexus 4","iPad Pro","Nexus 5","iPad Pro","Nexus 6","Nexus 7","Nexus 10","Galaxy S5","iPhone 13","iPad Mini",
+"Galaxy Note 3","iPad","Galaxy S8","iPad","iPhone 6","Galaxy S9+","iPad","iPhone 7","Galaxy S5","Nokia N9","iPhone 5","iPhone 12"];
 
-var number_1=0;
+var number_1=1;
 
 var status=false;
 
 var timer_1=0;
 
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+
 var fs=require("fs");
+const path = require('path');
 
-let xhr = new XMLHttpRequest()
+const alerts = fs.readFileSync(
+    path.resolve(__dirname, './iplist12.json'),
+    {'encoding': 'utf-8'}
+  );
 
-//xhr.open( 'GET',  'https://proxylist.geonode.com/api/proxy-list?limit=500&page=1&sort_by=lastChecked&sort_type=desc&country=TR' )
-
-//xhr.open("GET","http://free-proxy.cz/en/proxylist/country/TR/socks4/ping/all");
-
-xhr.onload = function() {
-    
-    if ( xhr.status == 200 ) {
-        
-      //  console.log( xhr.responseText );
-
-        //console.log(JSON.parse(xhr.responseText));
+        console.log(JSON.parse(alerts));
                
 
-      //  var delek_file=JSON.stringify(xhr.responseText);
+    
 
      
       (async () => {
@@ -70,29 +64,26 @@ xhr.onload = function() {
        setInterval(function() {
         if(status==true) {
           console.log("status :"+status);
+          console.log("number :"+number_1);
           number_1=parseInt(number_1)+1;
         }
         else {
           number_1=parseInt(number_1)+1;
         }
-       },120000);
+       },100000);
  
         async function eko() {
         console.log(status);
+        
         try {
         var dizin_length=parseInt(dizin.length)-1;
         var delek1=dizin[Math.floor(Math.random()*dizin_length)];
-       // console.log(delek1);
-       // const iPhone = puppeteer.devices[delek1];
-       console.log(JSON.parse(xhr.responseText).data[number_1].ip);
-          
-       /* if(await timer_1==2) {
-            number_1=parseInt(number_1)+1;
-            console.log("ip count :"+number_1);
-          }*/
-          //console.log(timer_1);
+     //   console.log(delek1);
+     //   const iPhone = puppeteer.devices[delek1];
+            console.log("ip :"+JSON.parse(alerts).table[number_1].ip+", type :"+JSON.parse(alerts).table[number_1].type);
+
          browser = await puppeteer.launch({
-          args: [   '--proxy-server=socks4://'+JSON.parse(xhr.responseText).data[number_1].ip+":"+JSON.parse(xhr.responseText).data[number_1].port,
+          args: [   '--proxy-server='+JSON.parse(alerts).table[number_1].type+'://'+JSON.parse(alerts).table[number_1].ip,
           '--ignore-certificate-errors',
           '--ignore-certificate-errors-spki-list ',
           '--disable-web-security',
@@ -100,17 +91,17 @@ xhr.onload = function() {
           '--disable-site-isolation-trials',
           "--no-sandbox", 
           "--disable-setuid-sandbox",
-          "--single-process",
+        "--single-process",
       "--no-zygote"],
-           headless:true,
+        //  headless:false,
           ignoreHTTPSErrors: true
         });
         const page = await browser.newPage();
         await page.authenticate();
-       // await page.emulate(iPhone);
+     //   await page.emulate(iPhone);
         
   
-        await page.goto('https://antalyasektorel.com.tr/',{waitUntil: "networkidle2"});
+        await page.goto('https://antalyasektorel.com.tr',{waitUntil: "networkidle2"});
        
         page.on('error', err=> {
           console.log('error happen at the page: ', err);
@@ -120,12 +111,10 @@ xhr.onload = function() {
           console.log('pageerror occurred: ', pageerr);
         })
         
-      
-      
+
         console.log(await page.title());
 
-        console.log(JSON.parse(xhr.responseText).data[number_1].ip);
-      
+
       
       status=true;
       
@@ -162,14 +151,8 @@ xhr.onload = function() {
       })();
 
         
-    } else {
-        
-        console.log( `Error: ${xhr.status}` )
-        
-    }
-}
+   
 
-xhr.send();
 
 async function scrollToBottom() {
      
@@ -183,7 +166,8 @@ async function scrollToBottom() {
     const timer = setInterval(() => {
       document.scrollingElement.scrollBy(0, distance);
       if (document.scrollingElement.scrollTop + window.innerHeight >= document.scrollingElement.scrollHeight) {
-      //  document.getElementsByClassName("product-btn")[0].click();
+        
+        //  document.getElementsByClassName("product-btn")[0].click();
         clearInterval(timer);
         resolve();
       }
@@ -191,4 +175,3 @@ async function scrollToBottom() {
   });
 }
 
-console.log("eko2");
